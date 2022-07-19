138478310c504814956b40fa9304f3f3ceb22242
Maria Carolina Boabaid <carolkboabaid@gmail.com>
2022-07-19 20:36:37 -0300

    Criação do ex012.js

--- ModuloD/ex011.js
+++ ModuloD/ex011.js
@@ -1,9 +1,12 @@
-var hora = 12
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são ${hora} horas.`)
-if (hora < 12){
+if (hora >= 06 && hora < 12){
     console.log("Bom dia!")
-} else if (18 > hora >= 12 ){
+}
+else if (hora >= 12 && hora < 18){
     console.log("Boa tarde!")
-} else{
+}
+else{
     console.log("Boa noite!")
 }
