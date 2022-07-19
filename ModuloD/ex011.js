cd7d67de6be4e3eaef08f496bbd0d6787b6fbdd2
Maria Carolina Boabaid <carolkboabaid@gmail.com>
2022-07-19 18:59:38 -0300

    Criação do ex010.html  ex011.html

---
+++ ModuloD/ex011.js
@@ -0,0 +1,9 @@
+var hora = 12
+console.log(`Agora são ${hora} horas.`)
+if (hora < 12){
+    console.log("Bom dia!")
+} else if (18 > hora >= 12 ){
+    console.log("Boa tarde!")
+} else{
+    console.log("Boa noite!")
+}
