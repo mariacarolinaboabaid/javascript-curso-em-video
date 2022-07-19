4a254308745ca81d0ca64fd9021a7b17b97e510a
Maria Carolina Boabaid <carolkboabaid@gmail.com>
2022-07-19 17:25:05 -0300

    Criação do ex009.html

--- ModuloD/ex008.js
+++ ModuloD/ex008.js
@@ -1,1 +1,5 @@
-console.log("O console está funcionando!")
+var velocidade = 60.5
+console.log(`A velocidade do carro está a ${velocidade} Km/h.`)
+if (velocidade >= 60){
+    console.log('Limite de velocidade ultrapassada. MULTADO!')
+}
